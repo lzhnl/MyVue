fb35223506bce3e3e03f9fbce82830c8e0e1b61d
zzz <1870184339@qq.com>
2021-05-10 16:19:46 +0800

    添加导航

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import  Vue from 'vue'
+import  Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state ={
+    count:0,
+    isLogin:false,
+}
+
+const mutations ={
+    add(state)
+    {
+        state.count +=1;
+    },
+    reduce(state)
+    {
+        state.count -= 1;
+    }
+}
+export default new Vuex.Store({
+    state,
+    mutations,
+})
